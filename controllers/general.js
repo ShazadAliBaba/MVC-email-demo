4b66138f81178c617ed60f8f314921876a2a85cb
ShazadAliBaba <shazadio@hotmail.com>
2021-02-27 10:43:59 -0500

    MVC email demo complete

--- controllers/general.js
+++ controllers/general.js
@@ -9,5 +9,77 @@ router.get("/", (req, res) => {
   });
 });
 
+// Browser gets the requested view
+router.get("/contact-us", (req, res) => {
+  res.render("general/contactUs", {
+    title: "contact Us",
+  });
+});
+// Receives the post back from our contact form
+router.post("/contact-us", (req, res) => {
+  console.log(req.body);
+  //res.send(req.body);
+
+  // long way
+  //const firstName = req.body.firstName;
+  //const (...)
+
+  // short way
+  // Contains all of the different properties that are submitted
+  const { firstName, lastName, email, message } = req.body;
+
+  // Server-side validation
+  let validationResults = {};
+  let passedValidation = true;
+  if (typeof firstName !== "string" || firstName === "") {
+    validationResults.firstName = "You must specify a first name.";
+    passedValidation = false;
+  } else if (firstName.length < 2) {
+    validationResults.firstName =
+      "The first name must be at least 2 characters.";
+    passedValidation = false;
+  }
+  if (passedValidation) {
+    // Send a mail message with SendGrid
+    const sgMail = require("@sendgrid/mail");
+
+    // We will protect our API key in an .env file; process.env.<KEY VARIABLE>
+    //sgMail.setApiKey("<key>"); // Not safe
+    sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+
+    /* An object that contains to/from email address, subject, and a body of the message 
+    that will get processed to submitter in HTML format */
+    const msg = {
+      to: "shazadio@hotmail.com",
+      from: "sali273@myseneca.ca",
+      subject: "Contact Us Form Submission",
+      html: `Visitor's Full Name: ${firstName} ${lastName}<br />
+              Visitor's Email Address: ${email}<br />
+              Visitor's message: ${message}<br />`,
+    };
+    // Asynchronously sends the email message and returns a promise (confirmation)
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.send("Success");
+      })
+      .catch((err) => {
+        console.log(`Error ${err}`);
+        res.send("Error");
+      });
+
+    // Returning the page by rendering it
+    res.send("Success");
+  } else {
+    res.render("general/contactUs", {
+      title: "contact Us",
+      // Passing this object as a message to the view
+      validationResults: validationResults,
+      // Passing the value to our contact handlebars to retain the value
+      values: req.body,
+    });
+  }
+});
+
 // Exports our router/controller
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Import modules
 const express = require("express");
 const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 // Create install of an app
 const app = express();
@@ -15,13 +16,20 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 
+// Set up body parser; middleware of our web app
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set up dotenv; injects key into process.env
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config/keys.env" });
+
 // Load controllers to load our modules
 const generalController = require("./controllers/general");
 // Set up a default route, but the URL path will be defined by our controller
 app.use("/", generalController);
 
-// Start listening (8080 optional here)
-const PORT = process.env.PORT || 8080;
+// Start listening (port 8080 defined in .env file); reads the value of API key variable
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Web server is up and running on port ${PORT}`);
